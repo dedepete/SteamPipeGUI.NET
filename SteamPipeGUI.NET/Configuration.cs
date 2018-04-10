2868f665b5efdf70e62eaf471cc878b42071927d
Igor Popov <dedepete@mail.ru>
2018-04-11 01:00:08 +0300

    Cleaned up.

--- SteamPipeGUI.NET/Configuration.cs
+++ SteamPipeGUI.NET/Configuration.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SteamPipeGUI.NET
+﻿namespace SteamPipeGUI.NET
 {
-    class Configuration
+    internal class Configuration
     {
         public string SteamLogin { get; set; }
+
         public string SteamCmdPath { get; set; }
+
         public object[] Branches { get; set; }
     }
 }
--- SteamPipeGUI.NET/Properties/AssemblyInfo.cs
+++ SteamPipeGUI.NET/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("18.4.10.87")]
-[assembly: AssemblyFileVersion("18.4.10.86")]
+[assembly: AssemblyVersion("18.4.10.88")]
+[assembly: AssemblyFileVersion("18.4.10.87")]
